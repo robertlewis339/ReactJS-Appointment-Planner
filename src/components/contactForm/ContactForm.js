54031d788a08add5ec4f34e348418c31946c09f5
Rob Lewis <robertlewis339@gmail.com>
2021-12-17 12:27:53 +0000

    Finishing touches

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -15,6 +15,7 @@ export const ContactForm = ({
         type="text"
         value={name}
         onChange={({ target }) => { setName(target.value) }}
+        placeholder="Name"
         required
       />
       <input
@@ -22,12 +23,14 @@ export const ContactForm = ({
         value={phone}
         pattern="(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)"
         onChange={({ target }) => { setPhone(target.value) }} 
+        placeholder="Phone Number"
         required
       />
       <input
         type="email"
         value={email}
         onChange={({ target }) => { setEmail(target.value) }} 
+        placeholder="Email"
         required
       />
       <input
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
+import { TileList } from '../../components/tileList/TileList';
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+
+export const AppointmentsPage = (props) => {
+  const appointments = props.appointments;
+  const contacts = props.contacts;
+  const addAppointment = props.addAppointment;
+
+  const [contact, setContact] = useState('');
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    // Add contact info and clear data 
+    addAppointment(contact, title, date, time);
+    // reseting values
+    setContact('');
+    setTitle('');
+    setDate('');
+    setTime('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+          title={title}
+          setTitle={setTitle}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+          contacts={contacts}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList objArr={appointments}/>
       </section>
     </div>
   );
